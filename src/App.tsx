797abda4b677dd97a7248a61d221f2bb0d1f3790
Dillon Redding <dillonredding@gmail.com>
2021-11-28 13:14:37 -0600

    adding external links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+// import './App.css';
+
+export enum IconStyle {
+  Solid = 'fas',
+  Brands = 'fab'
+}
+
+export enum IconSize {
+  Normal = '',
+  Large = 'fa-lg',
+  ExtraLarge = 'fa-2x'
+}
+
+export enum IconType {
+  FileIcon = 'file-icon',
+  Icon = 'icon',
+  PanelIcon = 'panel-icon'
+}
+
+export enum IconContainerSize {
+  Small = 'is-small',
+  Normal = '',
+  Medium = 'is-medium',
+  Large = 'is-large'
+}
+
+export interface IconProps {
+  name: string;
+  style?: IconStyle;
+  size?: IconSize;
+  type?: IconType;
+  containerSize?: IconContainerSize;
+  left?: boolean;
+}
+
+export const Icon = ({
+  name,
+  style = IconStyle.Solid,
+  size = IconSize.Normal,
+  type = IconType.Icon,
+  containerSize = IconContainerSize.Normal,
+  left = false
+}: IconProps) => (
+  <span className={`${type} ${containerSize} ${left ? 'is-left' : ''}`}>
+    <i className={`${style} fa-${name} ${size}`}></i>
+  </span>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav className="navbar is-spaced" role="navigation" aria-label="main navigation">
+        <div className="container">
+          <div className="navbar-brand">
+            <a
+              href="/#"
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-end">
+              <a
+                className="navbar-item"
+                href="https://github.com/dillonredding"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon name="github" style={IconStyle.Brands} size={IconSize.Large} />
+              </a>
+              <a
+                className="navbar-item"
+                href="https://twitter.com/dillon_redding"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon name="twitter" style={IconStyle.Brands} size={IconSize.Large} />
+              </a>
+              <a
+                className="navbar-item"
+                href="https://medium.com/@dillonredding"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon name="medium" style={IconStyle.Brands} size={IconSize.Large} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,7 +142,7 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      </header> */}
     </div>
   );
 }
