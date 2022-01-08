4bae262b23f8f1935dbcb34a7c9926fa2069014c
Dillon Redding <dillonredding@gmail.com>
2022-01-08 09:36:32 -0600

    updating e4p landing page

--- src/features/e4p/ExercisesForProgrammersSummary.tsx
+++ src/features/e4p/ExercisesForProgrammersSummary.tsx
@@ -1,11 +1,14 @@
 export function ExercisesForProgrammersSummary() {
   return (
-    <p className="is-size-5">
-      These are my solutions to problems from the book{' '}
-      <a href="https://pragprog.com/titles/bhwb/exercises-for-programmers/" target="_blank" rel="noreferrer">
-        Exercises for Programmers
-      </a>{' '}
-      by Brian P. Hogan.
-    </p>
+    <>
+      <h1 className="title">Exercises for Programmers</h1>
+      <p className="is-size-5">
+        My solutions to problems from{' '}
+        <a href="https://pragprog.com/titles/bhwb/exercises-for-programmers/" target="_blank" rel="noreferrer">
+          Exercises for Programmers
+        </a>{' '}
+        by Brian P. Hogan.
+      </p>
+    </>
   );
 }
